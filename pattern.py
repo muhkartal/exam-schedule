243e05f173773ab8c5e45f4d2c12f7edcc99ffa3
muhkartal <Mavi_servis@outlook.com>
2024-12-17 23:47:47 +0300

    Update pattern.py

--- pattern.py
+++ pattern.py
@@ -154,19 +154,6 @@ if os.path.exists(file_path):
 
         st.write("### Seçili Derslerin Programı")
 
-        st.markdown("<div class='pdf-button-container'>", unsafe_allow_html=True)
-        if st.button("📄 PDF Oluştur ve İndir"):
-            pdf_path = create_pdf(course_details_list)
-            st.markdown("<p class='pdf-success'>PDF başarıyla oluşturuldu!</p>", unsafe_allow_html=True)
-            with open(pdf_path, "rb") as file:
-                st.download_button(
-                    label="📥 PDF'yi indir",
-                    data=file,
-                    file_name="sinav_programi.pdf",
-                    mime="application/pdf"
-                )
-        st.markdown("</div>", unsafe_allow_html=True)
-
         for details in course_details_list:
             st.markdown(
                 f"""
